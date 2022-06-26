367d1b06e33283f4d69f5ca5f03c87cb60c068a6
EUNJUNG OH <e97214j@naver.com>
2022-06-26 21:09:57 +0900

    2022 Algorithm Simulation

---
+++ baekjoon/14470 전자레인지.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+answer = 0
+if  a <=0:
+  answer = a * c * -1
+  answer += d
+  answer += (e * b)
+else:
+  answer = e * (b-a)
+
+
+
+print(answer)
